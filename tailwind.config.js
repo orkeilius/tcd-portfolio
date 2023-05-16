4ef11eaf83948eb9b7e19136df366d4352f10766
orkeilius <63045634+orkeilius@users.noreply.github.com>
2023-05-16 16:27:27 +0100

    remade the headers from tcd.ie website

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'accent': '#0e73b8',
+        'accentHover':'#0f6eae',
+        'text': '#53565a'
       },
+      screens: {
+        'lg': '1180px'
+      }
     },
   },
   plugins: [],
