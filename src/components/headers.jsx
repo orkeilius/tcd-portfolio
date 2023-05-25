c30ad7568088acedd8f0834ec09ef8b2ea05e6d1
orkeilius <63045634+orkeilius@users.noreply.github.com>
2023-05-25 12:01:04 +0100

    add project link in nav for student/professor

--- src/components/headers.jsx
+++ src/components/headers.jsx
@@ -1,14 +1,42 @@
-import tcdLogo from "src/image/tcd-logo.png";
+import { useState, useEffect, useContext } from "react";
+import { SessionContext } from "./SessionProvider";
+import { supabase } from "../lib/supabaseClient";
 import useTranslation from "src/lib/TextString";
-import Login from "src/components/login"
+import tcdLogo from "src/image/tcd-logo.png";
+import Login from "src/components/login";
 
 export default function Home() {
-    const text = useTranslation()
-    var menuItem = [
-        { text: text['home'], link: "/" },
+    const text = useTranslation();
+    const session = useContext(SessionContext);
 
-    ];
+    const [menuItem, setMenuItem] = useState([
+        { text: text["home"], link: "/" },
+    ]);
 
+    useEffect(() => {
+        const getUserMetadata = async () => {
+            if (session == null) {
+                setMenuItem([{ text: text["home"], link: "/" }]);
+                return;
+            }
+            let { data, error } = await supabase
+                .from("role")
+                .select("role_role")
+                .eq("role_id", session.user.id);
+
+            if (error != null) {
+                console.log(error);
+                return;
+            }
+            if (["student", "professor"].includes(data[0]["role_role"])) {
+                setMenuItem([
+                    { text: text["home"], link: "/" },
+                    { text: text["project"], link: "project" },
+                ]);
+            }
+        };
+        getUserMetadata();
+    }, [session, text]);
     return (
         <>
             <header className="px-1 py-5 w-3/4 flex justify-between">
@@ -24,7 +52,7 @@ export default function Home() {
             </header>
             <nav className="bg-accent text-white pt-5 w-full">
                 <div className="px-1 mx-auto w-3/4">
-                    <h2 className="text-3xl font-light">{text['title']}</h2>
+                    <h2 className="text-3xl font-light">{text["title"]}</h2>
                     <div className="flex">
                         {menuItem.map((item) => (
                             <a
