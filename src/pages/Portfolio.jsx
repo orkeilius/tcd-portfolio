06fbcc7c154482b4832b1e696feadee06cdc05c0
orkeilius <63045634+orkeilius@users.noreply.github.com>
2023-05-26 13:41:00 +0100

    add db conncection to protfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,63 +1,82 @@
+import { SessionContext } from "src/components/SessionProvider";
+import { useEffect, useContext, useState } from "react";
+import { supabase } from "../lib/supabaseClient";
+import useTranslation from "src/lib/TextString";
+import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import { useRef, useState } from "react";
-import { faker } from "@faker-js/faker";
+
 import DownloadList from "src/components/preset/downloadList";
 import Comment from "src/components/preset/comment";
-import useTranslation from "src/lib/TextString";
 
 export default function Portfolio(props) {
     const text = useTranslation();
+    const navigate = useNavigate();
 
-    function getPortfolioData(portfolioId) {
+    const session = useContext(SessionContext);
+    async function getPortfolioData(portfolioId) {
         // Query file from db with props.postId
+        let { data, error } = await supabase
+            .from("portfolio")
+            .select("*,userMetadata(*)")
+            .eq("id", portfolioId);
 
-        //place holder
-        return {
-            firstName: faker.person.firstName(),
-            lastName: faker.person.lastName(),
-            title: faker.company.buzzPhrase(),
-            text: faker.lorem.paragraph({ min: 10, max: 100 }),
-        };
+        if (error != null) {
+            navigate("/");
+            console.error(error);
+            return;
+        }
+
+        if (data.length === 0) {
+            return null;
+        }
+
+        return {...data[0],};
     }
-    const handleTitleEdit = (event) => {
+    async function handleEdit(key,value, portfolioId) {
+        const { error } = await supabase
+            .from("portfolio")
+            .update({ [key]: value })
+            .eq("id", portfolioId);
+
+        if (error != null) console.error(error);
+
         setPortfolioData((commentData) => ({
             ...commentData,
-            title: event.target.value,
+            [key]: value,
         }));
     };
-    const handleTextEdit = (event) => {
-        event.target.style.height = "auto";
-        event.target.style.height = `${event.target.scrollHeight}px`;
-        setPortfolioData((commentData) => ({
-            ...commentData,
-            text: event.target.value,
-        }));
-    };
+    
 
     const { id } = useParams();
-    const [isAuthor, setIsAuthor] = useState(true);
-    const [portfolioData, setPortfolioData] = useState(getPortfolioData);
+    const [portfolioData, setPortfolioData] = useState(null);
+
+    useEffect(() => {
+        getPortfolioData(id).then((res) => setPortfolioData(res));
+    }, [id]);
+
+    if (portfolioData == null) {
+        return null;
+    }
+
+    const isAuthor = portfolioData.student_id === session.id;
     if (isAuthor) {
         setTimeout(() => {
             var textarea = document.getElementById("area");
             textarea.style.height = `${textarea.scrollHeight}px`;
         });
     }
+    console.log(portfolioData)
     return (
         <main>
-            <button
-                className="bg-green-300 mb-5"
-                onClick={() => setIsAuthor(!isAuthor)}
-            >
-                dev : is author : {isAuthor ? "true" : "false"}
-            </button>
             <br />
             {isAuthor ? (
                 <input
                     placeholder={text["title placeholder"]}
                     className="font-semibold text-3xl border-b border-black p-1 w-full"
                     value={portfolioData.title}
-                    onChange={handleTitleEdit}
+                    onChange={(event) =>
+                        handleEdit('name',event.target.value, id)
+                    }
                 />
             ) : (
                 <h1 className="font-semibold text-3xl border-b border-black p-1">
@@ -65,13 +84,11 @@ export default function Portfolio(props) {
                 </h1>
             )}
             <p className="font-thin ml-1">
-                {
-                    text["portfolio author"] +
+                {text["portfolio author"] +
                     " " +
-                    portfolioData.firstName +
+                    portfolioData.userMetadata.firstName +
                     " " +
-                    portfolioData.lastName
-                }
+                    portfolioData.userMetadata.lastName}
             </p>
             {isAuthor ? (
                 <textarea
@@ -79,7 +96,7 @@ export default function Portfolio(props) {
                     placeholder={text["text placeholder"]}
                     className="resize-none w-full m-1"
                     value={portfolioData.text}
-                    onChange={handleTextEdit}
+                    onChange={(event) => handleEdit('text',event.target.value, id)}
                 />
             ) : (
                 <p className=" p-1">{portfolioData.text}</p>
