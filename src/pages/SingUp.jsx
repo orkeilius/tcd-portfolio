d7a068c22503c945718f8a8dbf6ebfa40560cf30
orkeilius <63045634+orkeilius@users.noreply.github.com>
2023-05-30 14:46:22 +0100

    add locale for sing up

--- src/pages/SingUp.jsx
+++ src/pages/SingUp.jsx
@@ -1,9 +1,9 @@
+import { IoSync, IoCheckmarkCircle } from "react-icons/io5";
 import { supabase } from "../lib/supabaseClient";
-import singup_bg from "src/image/singup-bg.jpg";
 import { toast } from "react-toastify";
 import { useState } from "react";
-import { IoSync, IoCheckmarkCircle } from "react-icons/io5";
-
+import singup_bg from "src/image/singup-bg.jpg";
+import useTranslation from "src/lib/TextString";
 
 function InputEntry(props) {
     return (
@@ -11,18 +11,19 @@ function InputEntry(props) {
             className="my-8 mx-auto p-1 outline-none bg-slate-200 block text-center focus-visible:border-accent2 rounded-md border-2"
             {...props}
             required
-
         />
     );
 }
 
 export default function SingUp() {
+    const text = useTranslation();
+
     async function handleSingUp(event) {
         event.preventDefault();
         const args = event.target.elements;
 
         if (args.password.value !== args.password2.value) {
-            toast.error("missmatching password");
+            toast.error(text["password mismatch"]);
             return;
         }
         setFormStatus("pending");
@@ -39,58 +40,79 @@ export default function SingUp() {
         });
         if (error !== null) {
             console.error(error);
-            toast.error("an error occurred")
+            toast.error(text["error"]);
             setFormStatus("edit");
-        }
-        else {
+        } else {
             setFormStatus("fulfilled");
         }
     }
     const [formStatus, setFormStatus] = useState("edit"); // edit | pending | fulfilled
-    console.log(formStatus)
+    console.log(formStatus);
     return (
         <main className="grid md:grid-cols-2 w-full h-full items-center">
             <div>
                 <div className="mx-auto w-fit">
-                <div className={"transition-all overflow-hidden flex items-center justify-center " + (formStatus === "fulfilled" ? "max-h-[100px]" : "max-h-0")}>
+                    <div
+                        className={
+                            "transition-all overflow-hidden flex items-center justify-center " +
+                            (formStatus === "fulfilled"
+                                ? "max-h-[100px]"
+                                : "max-h-0")
+                        }
+                    >
                         <IoCheckmarkCircle className="fill-green-700 mr-1 mt-[2.5px]" />
-                        <p>account created - check your email</p>
+                        <p>{text["account created"]}</p>
                     </div>
-                    <div className={"transition-all overflow-hidden flex items-center justify-center " + (formStatus === "pending" ? "max-h-[100px]" : "max-h-0")}>
+                    <div
+                        className={
+                            "transition-all overflow-hidden flex items-center justify-center " +
+                            (formStatus === "pending"
+                                ? "max-h-[100px]"
+                                : "max-h-0")
+                        }
+                    >
                         <IoSync className="infiniteRotate mr-2" />
-                        <p>loading</p>
+                        <p>{text["loading"]}</p>
                     </div>
                 </div>
-                <form onSubmit={handleSingUp} className={"transition-all duration-700 overflow-hidden h-full max-h-0 border rounded-lg m-2 " + (formStatus === "edit" ? "max-h-[550px] border-white" : "max-h-0 border-black")}>
+                <form
+                    onSubmit={handleSingUp}
+                    className={
+                        "transition-all duration-700 overflow-hidden h-full max-h-0 border rounded-lg m-2 " +
+                        (formStatus === "edit"
+                            ? "max-h-[550px] border-white"
+                            : "max-h-0 border-black")
+                    }
+                >
                     <h1 className="text-6xl mb-16 text-center">Sing Up</h1>
                     <InputEntry
                         type="text"
-                        placeholder="firstName"
+                        placeholder={text["firstName"]}
                         name="firstName"
                         minlength="3"
                     />
                     <InputEntry
                         type="text"
                         name="lastName"
-                        placeholder="lastName"
+                        placeholder={text["lastName"]}
                         minlength="3"
                     />
                     <InputEntry type="email" name="email" placeholder="email" />
                     <InputEntry
                         type="password"
                         name="password"
-                        placeholder="password"
+                        placeholder={text["password"]}
                         minlength="8"
                     />
                     <InputEntry
                         type="password"
                         name="password2"
-                        placeholder="confirm password"
+                        placeholder={text["password2"]}
                     />
                     <input
                         className=" relative my-8 mx-auto block text-center py-1 w-[193px] text-lg transition duration-500 text-white bg-accent rounded-md border-accent border-2 hover:bg-white hover:text-accent"
                         type="submit"
-                        text="Sing Up"
+                        text={text["singUp"]}
                     />
                 </form>
             </div>
