85f853fd8ce6233f8f9761fce6f31b52117c7f74
orkeilius <63045634+orkeilius@users.noreply.github.com>
2023-06-02 15:02:59 +0100

    improve join page

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,24 +1,47 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { supabase } from "../lib/supabaseClient";
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
+import { SessionContext } from "src/components/SessionProvider";
+import useTranslation from "src/lib/TextString";
+import { toast } from "react-toastify";
 
 export default function Portfolio() {
     const { id, code } = useParams();
+    const session = useContext(SessionContext);
+    const navigate = useNavigate()
+    const text = useTranslation()
 
     async function join() {
         const { error } = await supabase.rpc("joinProject", {
             join_id: id,
             join_code: code,
         });
+        if (error === null) {
+            console.error(error);
+            toast.error(text["error"])
+            navigate("/")
+        }
+        else {
+            navigate("/project")
+        }
     };
 
     useEffect(() => {
-        join();
-    }, []);
+        if (['student','professor'].includes(session.role)) {
+            join();
+        }
+
+    }, [session.role]);
 
     return (
         <main>
-            <h2>redirecting ....</h2>
+            <h2 className="text-2xl my-5 text-center">
+            {['student','professor'].includes(session.role) ?
+                text['redirecting']
+                    :
+                text['join not connected']   
+            }
+            </h2>
         </main>
     );
 }
