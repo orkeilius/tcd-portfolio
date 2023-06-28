b953b1233f032b031b9a86c8901d22f5e56efa51
orkeilius <63045634+orkeilius@users.noreply.github.com>
2023-06-28 09:49:41 +0100

    fix imageCarousell spacing

--- src/components/downloadList/ImageCarousel.jsx
+++ src/components/downloadList/ImageCarousel.jsx
@@ -86,7 +86,7 @@ export default function ImageCarousel(props) {
         <>
             <div
                 className={
-                    "group transition-all duration-75 m-auto overflow-hidden " +
+                    "group transition-all duration-75 m-auto overflow-hidden mb-3 " +
                     (isFullscreen
                         ? "fixed w-full h-[100vh] top-0 left-0 z-50 bg-slate-400/50 "
                         : "relative h-96 border mx-5 rounded-lg")
