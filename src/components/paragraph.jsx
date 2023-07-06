1c8d36d4deb859254e9c539154a1f6e26accfb2d
orkeilius <63045634+orkeilius@users.noreply.github.com>
2023-07-06 13:28:46 +0100

    add hability to move paragraph

--- src/components/paragraph.jsx
+++ src/components/paragraph.jsx
@@ -3,7 +3,7 @@ import { supabase } from "src/lib/supabaseClient";
 import { useEffect, useState, useContext } from "react";
 import DownloadList from "src/components/downloadList";
 import useTranslation from "src/lib/TextString";
-
+import { IoCaretDown, IoCaretUp, IoTrashBin } from "react-icons/io5";
 export default function Paragraph(props) {
     async function getParagraphData(portfolioId) {
         let { data, error } = await supabase
@@ -57,6 +57,27 @@ export default function Paragraph(props) {
         getParagraphData(id);
     }
 
+    async function moveParagraph(paragraph, newPos) {
+        const { error1 } = await supabase
+            .from("paragraph")
+            .update({ position: paragraph.position })
+            .eq("position", newPos);
+        if (error1 != null) {
+            console.error(error1);
+            return;
+        }
+
+        let { error2 } = await supabase
+            .from("paragraph")
+            .update({ position: newPos })
+            .eq("id", paragraph.id);
+        if (error2 != null) {
+            console.error(error2);
+            return;
+        }
+        getParagraphData(id)
+    }
+
     function resizeTextarea(event) {
         event.target.style.height = "auto";
         event.target.style.height = `${event.target.scrollHeight}px`;
@@ -86,9 +107,49 @@ export default function Paragraph(props) {
         <>
             {paragraphData.map((paragraph, index) => {
                 return (
-                    <div key={paragraph.position} className="my-4">
+                    <div key={paragraph.id} className="my-4 transition-all">
                         {props.isAuthor ? (
-                            <>
+                            <div className="group ">
+                                <div className="w-0 h-0">
+                                    <div className="group-hover:max-w-lg hover:overflow-visible  md:max-w-0 relative h-fit transition-all duration-500 w-fit -translate-x-[130%] overflow-clip ">
+                                        {index != 0 && <IoCaretUp className="w-7 h-7 aspect-square mb-1 hover:scale-150 transition-all text-gray-500"
+                                            onClick={() => {
+                                                moveParagraph(
+                                                    paragraph,
+                                                    paragraph.position - 1
+                                                );
+                                            }}
+                                        />}
+                                        {index != paragraphData.length - 1 && <IoCaretDown
+                                            className="w-7 h-7 aspect-square mb-1 hover:scale-150 transition-all text-gray-500"
+                                            onClick={() => {
+                                                moveParagraph(
+                                                    paragraph,
+                                                    paragraph.position + 1
+                                                );
+                                            }}
+                                        />}
+                                        <IoTrashBin
+                                            className="w-7 h-7 aspect-square mb-1 hover:scale-150 transition-all text-red-600"
+                                            title={text["delete paragraph"]}
+                                            onClick={() => {
+                                                setConfirmPopUp(
+                                                    text[
+                                                        "paragraph confirm"
+                                                    ].replace(
+                                                        "{0}",
+                                                        paragraph.title
+                                                    ),
+                                                    () =>
+                                                        handleDelete(
+                                                            paragraph.position,
+                                                            id
+                                                        )
+                                                );
+                                            }}
+                                        />
+                                    </div>
+                                </div>
                                 <input
                                     placeholder={text["subtitle placeholder"]}
                                     className="font-semibold text-xl p-1 w-full hover:bg-gray-100 rounded-lg "
@@ -135,7 +196,7 @@ export default function Paragraph(props) {
                                         );
                                     }}
                                 />
-                            </>
+                            </div>
                         ) : (
                             <>
                                 <h1 className="font-semibold text-xl p-1 text-center sm:text-left">
