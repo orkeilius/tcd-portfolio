f1e0d4be52775cce928fa16c4c36b308397a624b
orkeilius <63045634+orkeilius@users.noreply.github.com>
2023-07-07 14:14:32 +0100

    update sing up test

--- cypress/e2e/test-singUp.cy.js
+++ cypress/e2e/test-singUp.cy.js
@@ -5,7 +5,7 @@ function fill(value) {
 
   cy.get('@form').find('input[placeholder="first name"]').type(value?.firstname ?? 'first name')
   cy.get('@form').find('input[placeholder="last name"]').type(value?.lastname ?? 'last name')
-  cy.get('@form').find('input[placeholder="email"]').type(value?.email ?? 'test@cypress.local')
+  cy.get('@form').find('input[placeholder="email"]').type(value?.email ?? 'invalid@cypress.local')
   cy.get('@form').find('input[placeholder="password"]').type(value?.password ?? 'securePassword_0')
   cy.get('@form').find('input[placeholder="confirm password"]').type(value?.password2 ?? 'securePassword_0')
 
@@ -19,7 +19,7 @@ describe('test sing up input', () => {
 
   })
 
-  it('input correspondind value', () => {
+  it('check input type', () => {
     cy.get('@form').find('input[placeholder="first name"][type="text"]')
     cy.get('@form').find('input[placeholder="last name"][type="text"]')
     cy.get('@form').find('input[placeholder="email"][type="email"]')
@@ -33,23 +33,26 @@ describe('test sing up input', () => {
   })
   it('full', () => {
     fill()
-
     cy.get('@form').submit()
-    cy.contains('account created')
+    cy.contains('email is already used')
   })
 
   const inputValue = ['firstname', 'lastname', 'email']
   inputValue.forEach(key => {
     it(`missing ${key}`, () => {
       fill({ [key]:'{del}' })
-      cy.get('@form').submit()
-      cy.contains('error')
+      cy.get('main').find('input:submit').click()
+      cy.wait(500)
+      cy.contains('loading').should('not.visible')
+      cy.contains('account created').should('not.visible')
     })
 
     it(`too small ${key}`, () => {
       fill({ [key]:'a' })
-      cy.get('@form').submit()
-      cy.contains('error')
+      cy.get('main').find('input:submit').click()
+      cy.wait(500)
+      cy.contains('loading').should('not.visible')
+      cy.contains('account created').should('not.visible')
     })
 
   })
@@ -57,21 +60,24 @@ describe('test sing up input', () => {
   it('wrong email', () => {
     fill({email:'not an email'})
 
-    cy.get('@form').submit()
-    cy.contains('error')
+    cy.get('main').find('input:submit').click()
+    cy.wait(500)
+      cy.contains('loading').should('not.visible')
+      cy.contains('account created').should('not.visible')
   })
 
   it('mismatching password', () => {
     fill({password:'wrong password'})
 
-    cy.get('@form').submit()
+    cy.get('main').find('input:submit').click()
     cy.contains('missmatching password')
   })
   it('password too small', () => {
     fill({password:'a',password2:'a'})
 
-    cy.get('@form').submit()
-    cy.contains('error')
+    cy.get('main').find('input:submit').click()
+    cy.wait(500)
+      cy.contains('loading').should('not.visible')
+      cy.contains('account created').should('not.visible')
   })
-
 })
